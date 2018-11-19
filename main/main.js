c403842d14a79dc30bcc1a56f112f48cc2ad4bc6
HOJO3 <joe.ho@oocl.com>
2018-11-19 16:45:01 +0800

    complete

--- main/main.js
+++ main/main.js
@@ -1,10 +1,27 @@
 // Write your cade below:
-function caclRemaindar() {};
+function caclRemaindar(x,y) {
+	return x%y;
+};
 
-function caclSum() {};
 
-function caclSumInConditon() {};
+function caclSum(array) {
+	sum = 0;
+	for (i = 0; i < array.length; i++){
+		sum+=array[i];
+	}
+	return sum;
+};
+	
 
+function caclSumInConditon(array,threshold) {	
+	sum = 0;
+	for (i = 0; i < array.length; i++){
+		if (array[i] < threshold){
+			sum+=array[i];
+		}
+	}
+	return sum;
+};
 module.exports = {
     caclRemaindar,
     caclSum,
